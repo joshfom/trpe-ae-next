1d4bd2878b101bb204b7da8d005b94a0787ba5bf
Joshua Fomubod <joshuafomum@gmail.com>
2025-07-09 13:56:30 +0400

    feat: integrate Google Tag Manager for enhanced tracking and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,8 @@ import {EdgeStoreProvider} from "@/db/edgestore";
 import {Toaster} from "sonner";
 import {SpeedInsights} from "@vercel/speed-insights/next";
 import {Analytics} from "@vercel/analytics/react"
-import {GoogleAnalytics} from '@next/third-parties/google'
 import Script from "next/script";
 import {cn} from "@/lib/utils";
-import GoogleTag from "@/components/GoogleTag";
 import Clarity from '@microsoft/clarity';
 
 const poppins = Poppins({
@@ -52,6 +50,14 @@ export default function RootLayout({
   return (
       <html lang="en" className="js" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MNQMSPX');`}
+        </Script>
         <meta name="google-site-verification" content="1PdN9Ng2af8MbSlor1keRIIXn_sM3nHkj2JPsWnyB1o"/>
         <meta name="next-head-count" content="3"/>
         <style dangerouslySetInnerHTML={{ 
@@ -67,6 +73,11 @@ export default function RootLayout({
         }} />
       </head>
       <body className={cn(poppins.className, playfairDisplay.variable, 'bg-slate-100 px-6 xl:px-0')}>
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNQMSPX"
+                height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe>
+      </noscript>
       <SpeedInsights/>
       <Analytics/>
       <NextTopLoader
@@ -80,10 +91,6 @@ export default function RootLayout({
           richColors
       />
 
-      <GoogleTag GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GOOGLE_TAG!}/>
-      <GoogleAnalytics
-          gaId="452930362"
-      />
       <Script
           id="facebook-pixel"
           strategy="afterInteractive"
