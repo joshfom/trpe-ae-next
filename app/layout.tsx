c0d5488b20ceaa404a8ec433d7e8546cf089153b
Joshua Fomubod <joshuafomum@gmail.com>
2025-07-09 14:59:08 +0400

    refactor: remove Clarity and Vercel analytics, integrate Google Analytics 4 for tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,8 @@ import "./tiptap-spacing.css";
 import NextTopLoader from 'nextjs-toploader';
 import {EdgeStoreProvider} from "@/db/edgestore";
 import {Toaster} from "sonner";
-import {SpeedInsights} from "@vercel/speed-insights/next";
-import {Analytics} from "@vercel/analytics/react"
 import Script from "next/script";
 import {cn} from "@/lib/utils";
-import Clarity from '@microsoft/clarity';
 
 const poppins = Poppins({
   weight: ['400', '500', '600'],
@@ -44,19 +41,21 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-    const projectId = process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID!;
-    Clarity.init(projectId);
   return (
       <html lang="en" className="js" suppressHydrationWarning>
       <head>
-        {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MNQMSPX');`}
+        {/* Google Analytics 4 */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-KYYZEMLWMT`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KYYZEMLWMT');
+          `}
         </Script>
         <meta name="google-site-verification" content="1PdN9Ng2af8MbSlor1keRIIXn_sM3nHkj2JPsWnyB1o"/>
         <meta name="next-head-count" content="3"/>
@@ -73,13 +72,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         }} />
       </head>
       <body className={cn(poppins.className, playfairDisplay.variable, 'bg-slate-100 px-6 xl:px-0')}>
-      {/* Google Tag Manager (noscript) */}
-      <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNQMSPX"
-                height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe>
-      </noscript>
-      <SpeedInsights/>
-      <Analytics/>
       <NextTopLoader
           color="#374151"
       />
