71ad0e12a69d460d695ec931e9afac55106e16d0
Joshua Fomubod <joshuafomum@gmail.com>
2025-07-17 17:44:40 +0400

    Improve google tag manager script

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,15 +44,12 @@ export default function RootLayout({
   return (
       <html lang="en" className="js" suppressHydrationWarning>
       <head>
-        {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-  var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-  j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id=GTM-MNQMSPX'+dl;
-  f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-MNQMSPX');`}
-        </Script>
-        
+   
+   {/* Google Tag Manager */}
+   <Script id="gtm-init" strategy="beforeInteractive" dangerouslySetInnerHTML={{
+  __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MNQMSPX');`
+}}/>
+        {/* End Google Tag Manager */}
         <meta name="google-site-verification" content="1PdN9Ng2af8MbSlor1keRIIXn_sM3nHkj2JPsWnyB1o"/>
         <meta name="next-head-count" content="3"/>
         <style dangerouslySetInnerHTML={{ 
@@ -66,9 +63,12 @@ export default function RootLayout({
             html.no-js [data-js-required="true"] { display: none !important; }
           `
         }} />
+
+
+        {/* End Google Tag Manager */}
       </head>
       <body className={cn(poppins.className, playfairDisplay.variable, 'bg-slate-100 xl:px-0')}>
-      {/* Google Tag Manager (noscript) */}
+
       <noscript>
         <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNQMSPX"
                 height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe>
